bcda72da7851e92aee8ac3e4c5334c447a025c36
Lasse Møldrup <lasse.moeldrup@gmail.com>
2023-07-18 09:39:27 +0200

    Derive Deserialize for database type

--- examples/some-verifier/src/lib.rs
+++ examples/some-verifier/src/lib.rs
@@ -1,6 +1,6 @@
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Default, Debug)]
+#[derive(Serialize, Deserialize, Default, Debug)]
 pub struct Verified {
     pub telegram_id: Option<u64>,
     pub discord_id: Option<u64>,
