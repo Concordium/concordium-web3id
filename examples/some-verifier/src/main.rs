caa4cad23a9a4591b99c41153d13fb154d77b3d5
Lasse Møldrup <lasse.moeldrup@gmail.com>
2023-07-20 11:32:31 +0200

    Add tentative support for fetching Telegram usernames

--- examples/some-verifier/src/main.rs
+++ examples/some-verifier/src/main.rs
@@ -7,6 +7,12 @@ use axum::routing::get;
 use axum::{Json, Router};
 use clap::Parser;
 use futures::{future, TryFutureExt};
+use rust_tdlib::client::tdlib_client::TdJson;
+use rust_tdlib::client::{
+    AuthStateHandlerProxy, ClientIdentifier, ConsoleClientStateHandlerIdentified,
+};
+use rust_tdlib::tdjson;
+use rust_tdlib::types::{GetUser, TdlibParameters};
 use serde::Deserialize;
 use some_verifier::{Platform, Verification};
 
@@ -23,6 +29,24 @@ struct App {
         env = "DISCORD_BOT_TOKEN"
     )]
     discord_bot_token: String,
+    #[clap(
+        long = "telegram-api-id",
+        help = "Telegram API ID for looking up usernames.",
+        env = "TELEGRAM_API_ID"
+    )]
+    telegram_api_id: i32,
+    #[clap(
+        long = "telegram-api-hash",
+        help = "Telegram API hash for looking up usernames.",
+        env = "TELEGRAM_API_HASH"
+    )]
+    telegram_api_hash: String,
+    #[clap(
+        long = "telegram-bot-token",
+        help = "Telegram bot token for looking up usernames.",
+        env = "TELEGRAM_BOT_TOKEN"
+    )]
+    telegram_bot_token: String,
     #[clap(
         long = "db",
         default_value = "host=localhost dbname=some-verifier user=postgres password=password port=5432",
@@ -49,6 +73,7 @@ struct App {
 #[derive(Clone)]
 struct AppState {
     client: reqwest::Client,
+    tdlib_client: Arc<rust_tdlib::client::Client<TdJson>>,
     discord_bot_token: Arc<String>,
     database: Arc<Database>,
 }
@@ -64,14 +89,42 @@ async fn main() -> anyhow::Result<()> {
             .with(app.log_level)
             .init();
     }
+    tdjson::set_log_verbosity_level(1);
 
     tracing::info!("Connecting to database...");
     let database = Database::connect(app.db_config).await?;
+
+    tracing::info!("Starting Telegram client...");
+    let tdlib_params = TdlibParameters::builder()
+        .database_directory("tddb")
+        .use_test_dc(false)
+        .api_id(app.telegram_api_id)
+        .api_hash(app.telegram_api_hash)
+        .system_language_code("en")
+        .device_model("Desktop")
+        .system_version("Unknown")
+        .application_version(env!("CARGO_PKG_VERSION"))
+        .enable_storage_optimizer(true)
+        .build();
+
+    let tdlib_client = rust_tdlib::client::Client::builder()
+        .with_tdlib_parameters(tdlib_params)
+        .with_client_auth_state_handler(ConsoleClientStateHandlerIdentified::new(
+            ClientIdentifier::BotToken(app.telegram_bot_token),
+        ))
+        .build()?;
+
+    let mut worker = rust_tdlib::client::Worker::builder()
+        .with_auth_state_handler(AuthStateHandlerProxy::new_with_encryption_key("".into()))
+        .build()?;
+    worker.start();
+    let tdlib_client = worker.bind_client(tdlib_client).await?;
 
     let client = reqwest::Client::new();
 
     let state = AppState {
         client,
+        tdlib_client: Arc::new(tdlib_client),
         discord_bot_token: Arc::new(app.discord_bot_token),
         database: Arc::new(database),
     };
@@ -91,8 +144,7 @@ async fn main() -> anyhow::Result<()> {
     let socket = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), app.api_port);
     axum::Server::bind(&socket)
         .serve(router.into_make_service())
-        .await
-        .unwrap();
+        .await?;
 
     Ok(())
 }
@@ -146,7 +198,23 @@ async fn get_username(state: &AppState, account: &Account) -> anyhow::Result<Str
 
     let id = account.id as u64;
     match account.platform {
-        Platform::Telegram => Ok((id as u64).to_string()),
+        // TODO: This is unreliable if the user deletes their chat with the bot
+        Platform::Telegram => {
+            let user = state
+                .tdlib_client
+                .get_user(GetUser::builder().user_id(account.id).build())
+                .await?;
+            let name = if user.username().is_empty() {
+                if user.last_name().is_empty() {
+                    format!("{}", user.first_name())
+                } else {
+                    format!("{} {}", user.first_name(), user.last_name())
+                }
+            } else {
+                user.username().clone()
+            };
+            Ok(name)
+        }
         Platform::Discord => {
             const API_ENDPOINT: &'static str = "https://discord.com/api/v10";
             let user = state
