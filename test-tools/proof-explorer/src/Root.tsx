ff2cf5209d5f53055c4581ed2fc87ced4f0b32b4
Aleš Bizjak <ab@concordium.com>
2023-08-17 22:59:45 +0200

    Add placeholder text.

--- test-tools/proof-explorer/src/Root.tsx
+++ test-tools/proof-explorer/src/Root.tsx
@@ -895,7 +895,12 @@ export default function ProofExplorer() {
                     </div>
                     <div className="bg-success mb-3 p-3 text-white">
                         <p> List of allowed issuers, e.g., 5916,5830 </p>
-                        <input className="my-1" onChange={onIssuersChange} value={issuers.toString()} />
+                        <input
+                            className="my-1"
+                            placeholder="5916,5830"
+                            onChange={onIssuersChange}
+                            value={issuers.toString()}
+                        />
                         {issuersDisplay}
                     </div>
                     <div>
