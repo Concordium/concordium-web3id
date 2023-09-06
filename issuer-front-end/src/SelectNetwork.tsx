6ea0ae6e160bff879f9e38cf67ed845cc4df0562
Aleš Bizjak <ab@concordium.com>
2023-09-06 21:49:09 +0200

    More text.

--- issuer-front-end/src/SelectNetwork.tsx
+++ issuer-front-end/src/SelectNetwork.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 /* eslint-disable no-alert */
 import React, { ChangeEvent, Dispatch, SetStateAction, useEffect } from 'react';
+import { MODULE_REFERENCE_CREDENTIAL_REGISTRY } from './constants';
 
 interface ConnectionProps {
     setIsTestnet: Dispatch<SetStateAction<boolean | undefined>>;
@@ -34,19 +35,25 @@ export default function SelectNetwork(props: ConnectionProps) {
 
     return (
         <>
-            <label className="field">
-                Select Network:
-                <br />
-                <br />
-                <select name="write" id="write" onChange={changeDropDownHandler}>
-                    <option value="choose" disabled selected>
-                        Choose
-                    </option>
-                    <option value="Testnet">Testnet</option>
-                    <option value="Mainnet">Mainnet</option>
-                </select>
-            </label>
+            <p>
+                {' '}
+                The next step in creating the issuer is to create a smart contract that will act as the registry of
+                credentials. In order to do this you need to connect the Concordium Wallet for Web. Please select the
+                network onto which you wish to deploy the new registry contract.
+            </p>
+            <p>
+                The registry contract will be created from the smart contract module{' '}
+                <strong>{MODULE_REFERENCE_CREDENTIAL_REGISTRY}</strong>.
+            </p>
+
             <br />
+            <select name="write" id="write" onChange={changeDropDownHandler}>
+                <option value="choose" disabled selected>
+                    Choose
+                </option>
+                <option value="Testnet">Testnet</option>
+                <option value="Mainnet">Mainnet</option>
+            </select>
         </>
     );
 }
