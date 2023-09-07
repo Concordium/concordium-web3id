002dc94f156fb5e04ded31ff26e8da36900ed6ef
Søren Bruus Zeppelin <soerenbf@gmail.com>
2023-09-07 09:21:50 +0200

    Configure platform to build for through env

--- examples/some-issuer/frontend/src/telegram/App.tsx
+++ examples/some-issuer/frontend/src/telegram/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import reactLogo from '../assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import '../App.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Telegram</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
--- examples/some-issuer/frontend/vite.config.ts
+++ examples/some-issuer/frontend/vite.config.ts
@@ -1,7 +1,45 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { replaceCodePlugin } from "vite-plugin-replace";
+
+type PlatformConfig = {
+  port: number;
+};
+
+const enum Platform {
+  Telegram = 'telegram',
+  Discord = 'discord',
+}
+
+const SUPPORTED_PLATFORMS = [Platform.Discord, Platform.Telegram];
+
+const platform: Platform = process.env.VITE_SOME_ISSUER_PLATFORM as Platform;
+
+if (!SUPPORTED_PLATFORMS.includes(platform)) {
+  throw new Error(`Please specify environment variable "VITE_SOME_ISSUER_PLATFORM" to be one of: ${SUPPORTED_PLATFORMS.join(',')}`)
+}
+
+const configs: { [p in Platform]: PlatformConfig } = {
+  [Platform.Telegram]: {
+    port: 8080,
+  },
+  [Platform.Discord]: {
+    port: 8081,
+  },
+}
+
+const { port } = configs[platform];
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    replaceCodePlugin({ replacements: [{ from: "{{platform}}", to: platform }] })
+  ],
+  esbuild: false,
+  server: {
+    port,
+    strictPort: true,
+  },
 })
