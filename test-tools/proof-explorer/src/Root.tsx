09a1caa9e3fc1929062248555e202b09d7828bcb
Aleš Bizjak <ab@concordium.com>
2023-09-13 12:36:20 +0200

    Put source link in the header.

--- test-tools/proof-explorer/src/Root.tsx
+++ test-tools/proof-explorer/src/Root.tsx
@@ -910,10 +910,18 @@ export default function ProofExplorer() {
 
     return (
         <main className="container">
-            <nav className="navbar bg-black mb-3">
+            <nav className="navbar bg-black mb-3 justify-content-between">
                 <div className="container-fluid">
                     <a className="navbar-brand text-white" href="#">
-                        {`Proof explorer ${version}.`}
+                        {`Proof explorer ${version} `}
+                    </a>
+                    <a
+                        className="navbar-text link-primary"
+                        target="_blank"
+                        rel="noreferrer"
+                        href={`https://github.com/Concordium/concordium-web3id/tree/main/test-tools/proof-explorer`}
+                    >
+                        (source code)
                     </a>
                 </div>
             </nav>
@@ -1108,16 +1116,6 @@ export default function ProofExplorer() {
 
                     <hr />
                     <Statement inner={statement} new_statement={new_statement} />
-                </div>
-                <div className="text-center">
-                    <a
-                        className="link"
-                        target="_blank"
-                        rel="noreferrer"
-                        href={`https://github.com/Concordium/concordium-web3id/tree/main/test-tools/proof-explorer`}
-                    >
-                        Source code
-                    </a>
                 </div>
                 <br />
                 <br />
