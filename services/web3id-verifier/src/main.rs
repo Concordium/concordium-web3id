816a78e5327d2beea87d59ee327ace7adf720942
Rob Squire <squirer@tcd.ie>
2025-11-05 11:59:10 +0000

    fix: cleanup verifier main

--- services/web3id-verifier/src/main.rs
+++ services/web3id-verifier/src/main.rs
@@ -1,27 +1,5 @@
-use anyhow::Context;
-use axum::{
-    extract::rejection::JsonRejection,
-    http::{self, StatusCode},
-    routing::{get, post},
-    Json, Router,
-};
-use axum_prometheus::PrometheusMetricLayerBuilder;
 use clap::Parser;
-use concordium_rust_sdk::{
-    contract_client::CredentialStatus,
-    id::{constants::ArCurve, types::GlobalContext},
-    types::hashes::BlockHash,
-    v2::{self, BlockIdentifier, Scheme},
-    web3id::{
-        self, did::Network, CredentialLookupError, Presentation, PresentationVerificationError,
-        Web3IdAttribute,
-    },
-};
-use futures::{Future, FutureExt};
 use web3id_verifier::{configuration::Cli, service};
-use std::sync::Arc;
-use tonic::transport::ClientTlsConfig;
-use tower_http::trace::{DefaultMakeSpan, DefaultOnResponse};
 
 
 #[tokio::main]
@@ -51,86 +29,6 @@ async fn main() -> anyhow::Result<()> {
         cli.request_timeout >= 1000,
         "Request timeout should be at least 1s."
     );
-
-    /* 
-    let endpoint = if cli.endpoint.uri().scheme() == Some(&Scheme::HTTPS) {
-        cli.endpoint
-            .tls_config(ClientTlsConfig::new())
-            .context("Unable to construct TLS configuration for Concordium API.")?
-    } else {
-        cli.endpoint
-    };
-
-    // Make it 500ms less than request timeout to make sure we can fail properly
-    // with a connection timeout in case of node connectivity problems.
-    let node_timeout = std::time::Duration::from_millis(cli.request_timeout - 500);
-
-    let endpoint = endpoint
-        .connect_timeout(node_timeout)
-        .timeout(node_timeout)
-        .http2_keep_alive_interval(std::time::Duration::from_secs(300))
-        .keep_alive_timeout(std::time::Duration::from_secs(10))
-        .keep_alive_while_idle(true);
-
-    let mut client = v2::Client::new(endpoint)
-        .await
-        .context("Unable to establish connection to the node.")?;
-
-    let params = client
-        .get_cryptographic_parameters(BlockIdentifier::LastFinal)
-        .await
-        .context("Unable to get cryptographic parameters.")?
-        .response;
-
-    let state = State {
-        client,
-        network: cli.network,
-        params: Arc::new(params),
-    };
-    */
-    // build routes
-    /* 
-    let server = Router::new()
-        .route("/v0/verify", post(verify_presentation))
-        .route("/v0/health", get(health))
-        .with_state(state)
-        .layer(
-            tower_http::trace::TraceLayer::new_for_http()
-                .make_span_with(DefaultMakeSpan::new().include_headers(cli.log_headers))
-                .on_response(DefaultOnResponse::new().include_headers(cli.log_headers)),
-        )
-        .layer(tower_http::timeout::TimeoutLayer::new(
-            std::time::Duration::from_millis(cli.request_timeout),
-        ))
-        .layer(tower_http::limit::RequestBodyLimitLayer::new(100_000)) // at most 100kB of data.
-        .layer(tower_http::cors::CorsLayer::permissive().allow_methods([http::Method::POST]))
-        .layer(prometheus_layer);
-
-    let shutdown_signal = set_shutdown()?;
-    let server_handle = tokio::spawn(async move {
-        axum::Server::bind(&cli.listen_address)
-            .serve(server.into_make_service())
-            .with_graceful_shutdown(shutdown_signal)
-            .await
-    });
-
-    if let Some(prometheus_handle) = prometheus_handle {
-        tokio::select! {
-            val = prometheus_handle => {
-                val.context("Prometheus task panicked.")?.context("Prometheus server crashed.")?;
-            }
-            val = server_handle => {
-                val.context("Server task panicked.")?.context("Server crashed.")?;
-            }
-        }
-    } else {
-        server_handle
-            .await
-            .context("Server task join error.")?
-            .context("Server crashed.")?;
-    }
-    */
-
 
     service::run_service(cli).await
 
